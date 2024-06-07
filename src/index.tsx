f9aaf39b79e598ceb1c27edf687b6f87c60416a7
unknown <C25Kieran.McCauley@afacademy.af.edu>
2024-06-07 14:54:10 -0400

    finalized dark mode button

--- src/index.tsx
+++ src/index.tsx
@@ -21,7 +21,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <MantineProvider theme={theme}>
+  <MantineProvider theme={theme} defaultColorScheme='dark'>
     <React.StrictMode>
       <UserRoleProvider>
         <Router>
