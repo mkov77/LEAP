5e8209ebd2b253c5ec74f39121ec8e7914a104c5
michaela <c25michaela.kovalsky@afacademy.af.edu>
2024-06-18 14:56:18 -0400

    undoing file deletion

---
+++ src/data/sections.ts
@@ -0,0 +1,25 @@
+import { type Section } from '../pages/landingPage';
+
+// Sections dummy data
+export const sections: Section[] = [
+  {
+    sectionid: 'M6A',
+    isonline: true
+  },
+  {
+    sectionid: 'T6B',
+    isonline: true
+  },
+  {
+    sectionid: 'M6C',
+    isonline: false
+  },
+  {
+    sectionid: 'T6A',
+    isonline: false
+  },
+  {
+    sectionid: 'T4A',
+    isonline: false
+  }
+]
